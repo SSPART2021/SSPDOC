6140ce211378a8fa12ddede18ebc9b91943e0a4e
SSPART2021 <82297276+SSPART2021@users.noreply.github.com>
2022-09-29 15:53:11 +0300

    Add files via upload

--- page.js
+++ page.js
@@ -22,64 +22,10 @@ var onDocumentLoad = function ( event ) {
 
 	text = text.replace( /\[name\]/gi, name );
 	text = text.replace( /\[path\]/gi, path );
-	text = text.replace( /\[page:([\w\.]+)\]/gi, "[page:$1 $1]" ); // [page:name] to [page:name title]
-	text = text.replace( /\[page:\.([\w\.]+) ([\w\.\s]+)\]/gi, "[page:" + name + ".$1 $2]" ); // [page:.member title] to [page:name.member title]
-	text = text.replace( /\[page:([\w\.]+) ([\w\.\s]+)\]/gi, "<a href=\"javascript:window.parent.goTo('$1')\" title=\"$1\">$2</a>" ); // [page:name title]
-	// text = text.replace( /\[member:.([\w]+) ([\w\.\s]+)\]/gi, "<a href=\"javascript:window.parent.goTo('" + name + ".$1')\" title=\"$1\">$2</a>" );
-
-	text = text.replace( /\[(?:member|property|method):([\w]+)\]/gi, "[member:$1 $1]" ); // [member:name] to [member:name title]
-	text = text.replace( /\[(?:member|property|method):([\w]+) ([\w\.\s]+)\]/gi, "<a href=\"javascript:window.parent.goTo('" + name + ".$2')\" target=\"_parent\" title=\"" + name + ".$2\" class=\"permalink\">#</a> .<a href=\"javascript:window.parent.goTo('$1')\" title=\"$1\" id=\"$2\">$2</a> " );
-
-	text = text.replace( /\[link:([\w|\:|\/|\.|\-|\_]+)\]/gi, "[link:$1 $1]" ); // [link:url] to [link:url title]
-	text = text.replace( /\[link:([\w|\:|\/|\.|\-|\_|\(|\)|\#]+) ([\w|\:|\/|\.|\-|\_|\s]+)\]/gi, "<a href=\"$1\"  target=\"_blank\">$2</a>" ); // [link:url title]
-	text = text.replace( /\*([\w|\d|\"|\-|\(][\w|\d|\ |\-|\/|\+|\-|\(|\)|\=|\,|\.\"]*[\w|\d|\"|\)]|\w)\*/gi, "<strong>$1</strong>" ); // *
-
-	text = text.replace( /\[example:([\w\_]+)\]/gi, "[example:$1 $1]" ); // [example:name] to [example:name title]
-	text = text.replace( /\[example:([\w\_]+) ([\w\:\/\.\-\_ \s]+)\]/gi, "<a href=\"../examples/#$1\"  target=\"_blank\">$2</a>" ); // [example:name title]
-
 
 	document.body.innerHTML = text;
 
-	// handle code snippets formatting
+	// End of onDocumentLoad function
+};
 
-	var elements = document.getElementsByTagName( 'code' );
-
-	for ( var i = 0; i < elements.length; i ++ ) {
-
-		var element = elements[ i ];
-
-		//------text = element.textContent.trim();
-		//------ text = text.replace( /^\t\t/gm, '' );
-
-		//------element.textContent = text;
-
-	}
-
-	// SSP3D Online button
-
-	var button = document.createElement( 'div' );
-	button.id = 'button';
-	button.textContent = 'SSP';
-
-	button.addEventListener( 'click', function ( event ) {
-
-		//window.open( 'https://localhost/' + section + '/' + path + '.html' );
-		window.open( 'https://localhost/' );
-		//document.location="localhost";
-
-	}, false );
-
-	document.body.appendChild( button );
-
-	// Syntax highlighting
-
-	var styleBase = document.createElement( 'link' );
-	styleBase.href ='#';
-	styleBase.rel = 'stylesheet';
-
-	var styleCustom = document.createElement( 'link' );
-	styleCustom.href = '#';
-	styleCustom.rel = 'stylesheet';
-
-	document.head.appendChild( styleBase );
-	document.head.appendChild( styleCustom );
+document.addEventListener( 'DOMContentLoaded', onDocumentLoad, false );
